7d28390efb65c3d4dd9ce23845b46eb12c32d665
bluepra <parora9@wisc.edu>
2021-07-25 15:17:03 -0500

    started making gui

---
+++ gui.py
@@ -0,0 +1,13 @@
+import pygame
+pygame.init()
+
+width = 330
+height = 330
+screen = pygame.display.set_mode((width, height))
+
+game_over = False
+
+while not game_over:
+    pygame.display.update()
+
+
